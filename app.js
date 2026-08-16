02fdc39da289d99d85faa0f00192da6aa2c7e205
chihnitha <chihnithabobbala03@gmail.com>
2026-08-16 10:28:17 +0530

    Move frontend to public folder

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const pool = require('./db');
 
-const path = require('path');
 const app = express();
+app.use(express.static('public'));
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(express.static(__dirname));
+
 
 const JWT_SECRET = process.env.JWT_SECRET || 'dev-secret-change-this';
 const FALLBACK_ADMIN_USERNAME = process.env.ADMIN_USERNAME || 'Athiva_designs';
